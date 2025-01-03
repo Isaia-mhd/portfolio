e50baebca01c43454c7916c06531ea0550c83d92
Isaia-mhd <mohamedesaie21@gmail.com>
2025-01-03 20:25:02 +0100

    remove the icon

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -12,7 +12,7 @@
     <header class="w-full h-[50px] mt-3 mb-12 ">
         <div class="w-full h-full bg-slate-900 max-w-[400px] mx-auto md:max-w-[80%] rounded-lg px-6">
             <ul class=" w-full h-full flex justify-between items-center gap-9">
-                <div class="w-6 h-6"><a href="{{route("home")}}"><img src="icon.png" alt="logo"></a></div>
+                <div class="w-6 h-6"><a href="{{route("home")}}">Logo</a></div>
                 <div class="flex items-center gap-3 md:gap-6 lg:gap-9">
                     <li class="text-white text-sm font-thin"><a href="{{route("home")}}">Home</a></li>
                     <li class="text-white text-sm font-thin"><a href="{{route("project")}}">Project</a></li>
